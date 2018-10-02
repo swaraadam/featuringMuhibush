e7f5e92afa439f5d5214a2043dcbc4bccb521f26
muhibush <ukiacrew@gmail.com>
2018-10-02 16:50:14 +0700

    new version

--- app/src/main/java/com/adam/bimaswara/belajarkotlinv2/MainActivity.kt
+++ app/src/main/java/com/adam/bimaswara/belajarkotlinv2/MainActivity.kt
@@ -1,52 +1,59 @@
 package com.adam.bimaswara.belajarkotlinv2
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.view.View
-import android.widget.RelativeLayout
+import android.util.Log
+import android.widget.LinearLayout
 import android.widget.Toast
-import kotlinx.android.synthetic.main.abc_activity_chooser_view_list_item.view.*
 import kotlinx.android.synthetic.main.activity_main.*
-import org.jetbrains.anko.*
+import org.jetbrains.anko.matchParent
 import org.jetbrains.anko.recyclerview.v7.recyclerView
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.verticalLayout
 
 class MainActivity : AppCompatActivity() {
 
-    private var items: MutableList<Item> = mutableListOf()
+    var footballItem : MutableList<Football> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        MainActivityUI().setContentView(this)
+        setContentView(R.layout.activity_main)
 
         initData()
 
-        club_list.layoutManager = LinearLayoutManager(this)
-        club_list.adapter = RecyclerViewAdapter(this, items){
-            val toast = Toast.makeText(applicationContext,it.name,Toast.LENGTH_SHORT)
-            toast.show()
-        }
-    }
+        verticalLayout {
+            lparams(matchParent, matchParent)
+            orientation = LinearLayout.VERTICAL
 
-    class MainActivityUI : AnkoComponent<MainActivity> {
-        override fun createView(ui: AnkoContext<MainActivity>): View {
-
+            recyclerView {
+                lparams(matchParent, matchParent)
+                layoutManager = LinearLayoutManager(context)
+                adapter = FootballAdapter(footballItem){
+                    startActivity<SecondActivity>(SecondActivity.POSITIONEXTRA to it)
+                    val toast = Toast.makeText(context, it.nama, Toast.LENGTH_SHORT)
+                    toast.show()
+                }
+            }
         }
+
 
     }
 
     private fun initData(){
         val name = resources.getStringArray(R.array.club_name)
         val image = resources.obtainTypedArray(R.array.club_image)
-        items.clear()
+        val keterangan = resources.getStringArray(R.array.club_info)
 
-        for(i in name.indices){
-            items.add(Item(name[i],image.getResourceId(i,0)))
+        footballItem.clear()
+
+        for (i in name.indices){
+            footballItem.add(Football(name[i], image.getResourceId(i,0), keterangan[i] ))
         }
+        Log.e("info", footballItem.size.toString())
 
         image.recycle()
+
     }
+
 }
-
-
